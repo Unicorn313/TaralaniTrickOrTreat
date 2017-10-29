dc365e90c97f76fb6e0e89b9220f7b4de4f6c269
Unicorn313 <32229467+Unicorn313@users.noreply.github.com>
2017-10-28 15:06:31 -1000

    added project to remote

--- TaralaniTrickOrTreat/ViewController.swift
+++ TaralaniTrickOrTreat/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var knockKnockButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+        imageView.image=UIImage(named: "doorImage")
+        knockKnockButton.setTitle("Knock Knock", for:[])
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+   
     }
 
 
